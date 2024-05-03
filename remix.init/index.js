f527783dae131d173edf887fa3b983ba28f35458
dev-xo <devxo@mail.com>
2024-05-03 17:17:48 +0200

    chore: update remix-init

--- remix.init/index.js
+++ remix.init/index.js
@@ -114,12 +114,7 @@ async function updateProjectNameFromRiles(rootDirectory, APP_NAME) {
 }
 
 async function removeUnusedFiles(rootDirectory) {
-  const FILES_TO_REMOVE = [
-    'CODE_OF_CONDUCT.md',
-    'LICENSE',
-    'CONTRIBUTING.md',
-    'SECURITY.md',
-  ]
+  const FILES_TO_REMOVE = ['CODE_OF_CONDUCT.md', 'LICENSE', 'SECURITY.md']
 
   await Promise.all(
     FILES_TO_REMOVE.map((file) => fs.unlink(path.join(rootDirectory, file))),
